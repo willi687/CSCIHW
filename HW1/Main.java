c8dc6a3217d263274790433e05dbfdb95c4eec8e
Gregory J-ctr Walsh <gregory.j-ctr.walsh@faa.gov>
2021-09-16 16:51:01 -0400

    added comments to main

--- HW1/Main.java
+++ HW1/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Start of Main");
         // Create an object that will make a Pyramid (see Pyramid.java for more info)
+
+        // Create an object that implements a method which will take an name, age, and favorite color and will return a story (see Adlibs.java for more info)
+
+        // Create an object that implements a method which will  accept a string, an integer, and a char and will replace the char in the string at the given integer (See Replace.java for more info)
         
 
     }
