7c8452a8e6d8fa0877c7887930820d34ca65de6b
Gregory J-ctr Walsh <gregory.j-ctr.walsh@faa.gov>
2021-09-30 20:14:58 -0400

    changed formatting

--- HW1/Adlibs.java
+++ HW1/Adlibs.java
@@ -1,5 +1,5 @@
 package HW1;
-// make a method that accepts 3 Strings (Someones name, age, and favorite color and then returns a story about them)
+// make a method that accepts 2 Strings and 1 int (Someones name, age, and favorite color and then returns a story about them)
 // Hint1: The objects name is Adlibs and you require a name age and favoriteColor all in strings
 // Hint2: You will need to return a string. You can concat the variables and the strings together
 // Hint3: A sample story if you are lost can be as simple as "Hello! My name is ____, and I am ____, and my favorite  
