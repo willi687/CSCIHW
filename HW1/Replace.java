4d38182ffbdf258a42ad281b45c620b610b0fb28
walshg3 <walshg3@go.stockton.edu>
2021-10-12 18:01:22 -0400

    changed formatting on comments

--- HW1/Replace.java
+++ HW1/Replace.java
@@ -1,8 +1,9 @@
 package HW1;
 // Make a method that accepts a string and an integer and a char and will return the string with the char
 // replaced at the integer given
-// EX I MAKE BREAD, 2, T
-//    I TAKE BREAD
+// charReplace("I MAKE BREAD",2,T)
+// EX: I MAKE BREAD, 2, T
+//     I TAKE BREAD
 // remember computer counting starts at 0:
 // HELLO
 // 01234
