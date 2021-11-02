f892983595c2a5299b764a3f878f37c13211b517
walshg3 <walshg3@go.stockton.edu>
2021-11-02 00:29:52 -0400

    added comments for arrays hw

--- HW4/Arrays.java
+++ HW4/Arrays.java
@@ -24,18 +24,48 @@ public class Arrays {
 
     }
 
+    // Create a method that will brute force a password EX.
+    // bruteForce("ZZZZ")
+    // The program should then guess each char of the string and compare it to see if it is equal
+    // to the index of the alphabet char array below
+    // Output Example:
+    // Z
+    // ZZ
+    // ZZZ
+    // ZZZZ
+
+    // Hint 1: using a char array for the password is helpful here 
+    // Hint 2: using the method .toCharArray is a way to convert the password parameter to
+    // a char array as well
+    // From there creating for and while loops to compare the values is trivial
+
     public static String bruteForce(String password){
         String answer = "";
-        char[] passArr = new char[password.length()];
+        char[] alphabet = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        '!', '@', '#', '$', '%', '^', '&', '*', '(', ')','<','>','/','?'};
+    
+        return answer; 
+    }
 
-        return answer;
+    // Create a method that will sort an Array of integers from smallest to greatest.
+    // NOTE: You CANNOT use the built in Array.sort method
+    // Ex. 
+    // sorter([9,10,2,5,3,4,7,8]) -> [2,3,4,5,7,8,9,10]
+
+    // Hint 1: a temp Array is a helpful way to store variables for comparing 
+    // Hint 2: 2 nested For loops should be all thats needed for the swapping logic
+    public static int[] sorter(int[] nums){
+        int [] sortedArray = new int[nums.length];
+
+        return sortedArray;
     }
 
 
 
 
     public static void main(String[] args) {
-        
+
     }
 
     
