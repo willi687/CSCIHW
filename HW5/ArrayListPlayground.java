e5333362a0c288ae3d42b0057e9e5d57faeba6a7
walshg3 <walshg3@go.stockton.edu>
2021-11-16 17:15:47 -0500

    added HW5

---
+++ HW5/ArrayListPlayground.java
@@ -0,0 +1,39 @@
+package HW5;
+
+public class ArrayListPlayground {
+    public static void main(String[] args) {
+    // Question 1
+    // Write Java statements that do the following:
+    // a) Declare an arrayList nums of 15 elements of type int.
+    // b) Output the value of the tenth element of the array nums.
+    // c) Set the value of the 5th element of the array alpha to 99.
+    // d) Set the value of the 9th element of the array alpha to the sum of the 6th and 13th elements of the array nums.
+
+
+
+
+    // Question 2
+    // a) create an arrayList of Strings that contain each day of the week.(starting on monday)
+    // b) output each of the days of the week
+    // c) output the days of the week we have class
+    // d) output the days of the week that we have class 
+    // e) change the arrayList to start on Sunday
+
+
+    // Question 3 
+    // a) create an ArrayList and prompt the user for numbers to add to it until the number 0 is selected
+    // b) return the largest and smallest number
+    // c) return the ArrayList sorted smallest to largest
+    // d) take that ArrayList see if its size is divisable by 3 and then output the ArrayList in a matrix format
+    // NOTE: make the matrix n X 3 so it can be n rows by 3 columns 
+    // EX. ArrayList [1,2,3,4,5,6,7,8,9]
+    // 1 2 3
+    // 4 5 6
+    // 7 8 9
+    // NOTE: If the ArrayList is NOT divisable by 3 ask the user for more numbers and add them until it is
+    // ArrayList Size: 7
+    // Please enter 2 more numbers to create the matrix...
+    // 
+    }
+   
+}
